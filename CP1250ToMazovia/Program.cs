ef811e9ad4ee4b3f7e15d04e7b92d9cafcfc672b
Wit <witalian.zajac@gmail.com>
2018-09-24 10:55:11 +0200

    initial

---
+++ CP1250ToMazovia/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CP1250ToMazovia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = string.Empty;
+            string outputFile = string.Empty;
+
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Podaj ścieżki do plików!");
+                return;
+            }
+
+            inputFile  = args[0].Trim('"');
+
+            if (args.Length == 1)
+            {
+                outputFile = inputFile;
+            }
+            else
+            {
+                outputFile = args[1].Trim('"');
+            }
+
+            if (!File.Exists(inputFile))
+            {
+                Console.WriteLine("Plik wejściowy nie istnieje!");
+                return;
+            }
+
+            Encoding encoding = new MazoviaEncoding();
+            File.WriteAllText(outputFile, File.ReadAllText(inputFile, Encoding.Default), encoding); // CP: 667 790 991
+        }
+    }
+}
---
+++ CP1250ToMazoviaTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace CP1250ToMazoviaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = @"TestSample.txt";
+            string outputFile = "TestSample.out.txt";
+
+            Process.Start("CP1250ToMazovia.exe", string.Format("\"{0}\" \"{1}\"", inputFile, outputFile));
+        }
+    }
+}
